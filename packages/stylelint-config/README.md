2a19f093147e678ee31c6450582ce4b631b9cd6e
Lucas Banegas <conobanegas@gmail.com>
2022-08-04 16:27:55 -0300

    feat(stylelint-config): initial commit

---
+++ packages/stylelint-config/README.md
@@ -0,0 +1,3 @@
+# `@arfreelance/stylelint-config`
+
+> Package in experimental stage. Do not use for production.
---
+++ packages/stylelint-config/lib/index.js
@@ -0,0 +1,48 @@
+// @arfreelance/stylelint-config
+
+const _ = require("lodash");
+
+const standard = require("stylelint-config-standard-scss");
+const prettier = require("stylelint-config-prettier-scss");
+
+// Custom config
+// -----------------------------------------------------------------------------
+
+const custom = {
+    extends: [],
+    plugins: ["@arfreelance/stylelint-plugin"],
+    rules: {
+        "no-descending-specificity": null,
+        "no-duplicate-selectors": null,
+        "no-empty-source": null,
+        "value-keyword-case": null,
+        "rule-empty-line-before": ["always", { except: ["first-nested"] }],
+        "at-rule-empty-line-before": [
+            "always",
+            {
+                except: ["first-nested"],
+                ignoreAtRules: ["import", "else", "extend", "use"],
+            },
+        ],
+        "at-rule-no-vendor-prefix": null,
+        "media-feature-name-no-vendor-prefix": null,
+        "property-no-vendor-prefix": null,
+        "selector-no-vendor-prefix": null,
+        "value-no-vendor-prefix": null,
+        "scss/dollar-variable-empty-line-after": null,
+        "scss/dollar-variable-empty-line-before": null,
+        "order/properties-order": require("./order"),
+    },
+};
+
+// Custom config
+// -----------------------------------------------------------------------------
+
+const config = _.assign({}, custom, standard, prettier);
+config.extends = _.concat(custom.extends, standard.extends, prettier.extends);
+config.rules = _.assign({}, standard.rules, prettier.rules, custom.rules);
+
+// Exports
+// -----------------------------------------------------------------------------
+
+module.exports = config;
