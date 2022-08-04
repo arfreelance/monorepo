1ec0555da82fa4eba9bd7d709c863a83bd803755
Lucas Banegas <conobanegas@gmail.com>
2022-08-04 15:14:07 -0300

    feat(eslint-plugin): initial commit

---
+++ packages/eslint-plugin/lib/index.js
@@ -0,0 +1,27 @@
+// @arfreelance/eslint-plugin
+
+const _ = require("lodash");
+
+// Plugins
+// -----------------------------------------------------------------------------
+
+const plugins = {
+    import: require("eslint-plugin-import"),
+    node: require("eslint-plugin-n"),
+    promise: require("eslint-plugin-promise"),
+};
+
+// Rules
+// -----------------------------------------------------------------------------
+
+const rules = _.assign(
+    {},
+    plugins.import.rules,
+    plugins.node.rules,
+    plugins.promise.rules
+);
+
+// Exports
+// -----------------------------------------------------------------------------
+
+module.exports = { rules };
