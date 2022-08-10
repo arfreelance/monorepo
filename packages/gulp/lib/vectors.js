3a3951af4b0c9c033ccfb3b37666272ab8722232
Lucas Banegas <conobanegas@gmail.com>
2022-08-10 20:32:28 -0300

    feat(gulp): add webp

--- packages/gulp/lib/vectors.js
+++ packages/gulp/lib/vectors.js
@@ -2,15 +2,10 @@
 
 const { src, dest } = require("gulp");
 const imagemin = require("gulp-imagemin");
-const webp = require("gulp-webp");
 
 // Build
 // -----------------------------------------------------------------------------
 
 module.exports = (source, target) => {
-    return src(source)
-        .pipe(imagemin())
-        .pipe(dest(target))
-        .pipe(webp())
-        .pipe(dest(target));
+    return src(source).pipe(imagemin()).pipe(dest(target));
 };
