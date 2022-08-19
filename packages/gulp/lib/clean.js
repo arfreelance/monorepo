3874341f2e6848cfdbe22c041dacfc8999699c6b
Lucas Banegas <conobanegas@gmail.com>
2022-08-18 23:45:53 -0300

    feat(gulp): add globby to clean

--- packages/gulp/lib/clean.js
+++ packages/gulp/lib/clean.js
@@ -1,10 +1,40 @@
 // @arfreelance/gulp
 
+const { promisify } = require("util");
+const globby = require("globby");
+const path = require("upath");
 const rimraf = require("rimraf");
+const clean = promisify(rimraf);
+
+// Options
+// -----------------------------------------------------------------------------
+
+const options = {
+    gitignore: true,
+};
+
+// Normalize
+// -----------------------------------------------------------------------------
+
+const normalize = (sources) => {
+    if (Array.isArray(sources)) {
+        sources = sources.map((source) => {
+            return path.normalizeSafe(source);
+        });
+    }
+
+    return sources;
+};
 
 // Exports
 // -----------------------------------------------------------------------------
 
-module.exports = (source, callback) => {
-    return rimraf(source, callback);
+module.exports = async (sources) => {
+    sources = normalize(sources);
+    sources = await globby(sources, options);
+    sources = sources.map((source) => {
+        return clean(source);
+    });
+
+    return Promise.all(sources);
 };
