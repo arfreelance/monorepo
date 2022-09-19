88ebadb4146c78cbf9ac45f75e66d9ce8b8a3a55
Lucas Banegas <conobanegas@gmail.com>
2022-09-18 22:37:59 -0300

    feat(package-lint): initial run process

---
+++ packages/package-lint/lib/index.js
@@ -0,0 +1,33 @@
+const { Command } = require("commander");
+const { pkglint } = require("@arfreelance/gulp");
+const pkg = require("../package.json");
+
+// Initialize program
+// -----------------------------------------------------------------------------
+
+const program = new Command();
+
+// Program data
+// -----------------------------------------------------------------------------
+
+program
+    .name("package-lint")
+    .description("Lint and fix package.json files")
+    .version(pkg.version);
+
+// Program process
+// -----------------------------------------------------------------------------
+
+async function run() {
+    program.argument("<globs...>", "files to lint").action(async (globs) => {
+        globs = globs.concat(["!**/node_modules/**/*"]);
+        await pkglint(globs);
+    });
+
+    await program.parseAsync(process.argv);
+}
+
+// Program run
+// -----------------------------------------------------------------------------
+
+run();
