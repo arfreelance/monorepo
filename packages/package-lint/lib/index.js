79667fcf37a0a6768fe950d787265ee48f9121b1
Lucas Banegas <lucas@argentinafreelance.com>
2022-09-20 02:49:20 -0300

    feat(package-lint): add gitignore support

--- packages/package-lint/lib/index.js
+++ packages/package-lint/lib/index.js
@@ -1,21 +1,15 @@
+const { promisify } = require("util");
+const { readFile } = require("fs");
 const { src, dest } = require("vinyl-fs");
+const findUp = require("find-up");
 const gulpEslint = require("gulp-eslint-new");
 const gulpif = require("gulp-if");
 const gulpPackage = require("./pkglint");
 const gulpPrettier = require("gulp-prettier");
 const gulpStylelint = require("@ronilaukkarinen/gulp-stylelint");
+const parse = require("parse-gitignore");
+const read = promisify(readFile);
 const stylelintFormatter = require("stylelint-formatter-pretty");
-
-// Ignore
-// -----------------------------------------------------------------------------
-
-const ignore = [
-    "!**/bower_components/**",
-    "!**/node_modules/**",
-    "!**/public/**",
-    "!**/vendor/**",
-    "!**/*.min.*",
-];
 
 // Conditions
 // -----------------------------------------------------------------------------
@@ -41,31 +35,60 @@ const onlyStylelint = (file) => {
     return exts.includes(file.extname);
 };
 
-// Options
+// Default ignores
 // -----------------------------------------------------------------------------
 
-const options = {
-    allowEmptyInput: true,
-    failAfterError: true,
-    fix: true,
-    reporters: [{ formatter: stylelintFormatter, console: true }],
+const getIgnores = async () => {
+    const ignores = [
+        "**/bower_components/**",
+        "**/node_modules/**",
+        "**/public/**",
+        "**/vendor/**",
+        "**/*.min.*",
+    ];
+
+    try {
+        const file = await findUp(".gitignore");
+        const data = file ? await read(file, "utf8") : null;
+        const globs = data ? parse.globs(data) : [];
+
+        globs.forEach((_) => {
+            _.patterns.forEach((glob) => {
+                ignores.push(_.type === "unignore" ? `!${glob}` : glob);
+            });
+        });
+
+        return [...new Set(ignores)];
+    } catch (error) {
+        throw new Error(error);
+    }
 };
 
 // Lint
 // -----------------------------------------------------------------------------
 
-module.exports = (globs) => {
-    const opts = { allowEmpty: true, base: "./", dot: true };
+module.exports = async (globs) => {
+    const options = {
+        src: {
+            allowEmpty: true,
+            base: "./",
+            dot: true,
+            ignore: await getIgnores(),
+        },
+        stylelint: {
+            allowEmptyInput: true,
+            failAfterError: true,
+            fix: true,
+            reporters: [{ formatter: stylelintFormatter, console: true }],
+        },
+    };
 
-    globs = globs.concat(ignore);
-    globs = [...new Set(globs)];
-
-    return src(globs, opts)
+    return src([...new Set(globs)], options.src)
         .pipe(gulpif(onlyPackage, gulpPackage()))
         .pipe(gulpif(onlyPrettier, gulpPrettier()))
         .pipe(gulpif(onlyEslint, gulpEslint.fix()))
         .pipe(gulpif(onlyEslint, gulpEslint.format()))
         .pipe(gulpif(onlyEslint, gulpEslint.failAfterError()))
-        .pipe(gulpif(onlyStylelint, gulpStylelint(options)))
+        .pipe(gulpif(onlyStylelint, gulpStylelint(options.stylelint)))
         .pipe(dest("./"));
 };
