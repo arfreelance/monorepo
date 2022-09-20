8c4f1aca4b15d4fb90d7173a42ed936f5d4637ce
Lucas Banegas <lucas@argentinafreelance.com>
2022-09-20 16:04:33 -0300

    feat(gulp): remove images minify and convert to webp if binary

--- packages/gulp/lib/images.js
+++ packages/gulp/lib/images.js
@@ -1,53 +1,25 @@
 // @arfreelance/gulp
 
-const { promisify } = require("util");
 const { src, dest } = require("gulp");
+const gulpif = require("gulp-if");
 const imagemin = require("gulp-imagemin");
-const map = require("flat-map").default;
-const read = require("gulp-scale-images/read-metadata");
-const readPromise = promisify(read);
-const scale = require("gulp-scale-images");
+const isBinary = require("isbinaryfile");
 const webp = require("gulp-webp");
 
-// Get variant
+// Pug condition
 // -----------------------------------------------------------------------------
 
-const getVariant = async (file, size) => {
-    const meta = await readPromise(file);
-    const maxWidth = Math.floor(meta.width * size);
-    const maxHeight = Math.floor(meta.height * size);
-    file.scale = { maxWidth, maxHeight };
-    return file;
-};
-
-// Mapper
-// -----------------------------------------------------------------------------
-
-const mapper = (file, callback) => {
-    const variants = Promise.all([
-        getVariant(file.clone(), 1),
-        getVariant(file.clone(), 0.75),
-        getVariant(file.clone(), 0.5),
-        getVariant(file.clone(), 0.25),
-    ]);
-
-    return callback(null, variants);
+const condition = (file) => {
+    return isBinary.isBinaryFileSync(file.contents);
 };
 
 // Build
 // -----------------------------------------------------------------------------
 
 module.exports.build = (source, target) => {
-    return src(source).pipe(imagemin()).pipe(dest(target));
-};
-
-// Minify
-// -----------------------------------------------------------------------------
-
-module.exports.minify = (source, target) => {
     return src(source)
-        .pipe(webp())
-        .pipe(map(mapper))
-        .pipe(scale())
-        .pipe(dest(target));
+        .pipe(imagemin())
+        .pipe(dest(target))
+        .pipe(gulpif(condition, webp()))
+        .pipe(gulpif(condition, dest(target)));
 };
